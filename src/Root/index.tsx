d6bcbe65ca2d33e004655c8082c40031e97fc389
Safar Ligal <weathermist@gmail.com>
2020-06-01 14:58:14 +0545

    Fix minor problems with eslint

--- src/Root/index.tsx
+++ src/Root/index.tsx
@@ -2,13 +2,10 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-interface Props {
-}
-
-function Root(props: Props) {
+function Root() {
     return (
         <BrowserRouter>
-            <App {...props} />
+            <App />
         </BrowserRouter>
     );
 }
